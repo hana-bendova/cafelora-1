96817e3678a54d28933a62765bb05f197835d22f
Hana Bendová <hana.bendova01@gmail.com>
2025-05-03 14:49:49 +0200

    4/5

---
+++ src/components/Drink/drink.jsx
@@ -0,0 +1,18 @@
+export const Drink = ({ id, name, ordered, image, layers }) => {
+  return (
+    <div className="drink">
+      <div className="drink__product">
+        <div className="drink__cup">
+          <img src={image} />
+        </div>
+        <div className="drink__info">
+          <h3>{name}</h3>
+        </div>
+      </div>
+      <form className="drink__controls">
+        <input type="hidden" className="order-id" value="0" />
+        <button className="order-btn">Objednat</button>
+      </form>
+    </div>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,14 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
+import '../components/Drink/drink.css';
+import '../components/Banner/banner.css';
+import '../components/Footer/footer.css';
+import '../components/Menu/menu.css';
+import '../components/Gallery/gallery.css';
+import '../components/Contact/contact.css';
+import '../components/Header/header.css';
+
 import { Header } from '../components/Header/header.jsx';
 import { Banner } from '../components/Banner/banner.jsx';
 import { Menu } from '../components/Menu/menu.jsx';
